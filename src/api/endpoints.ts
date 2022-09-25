78b4292585a181e2195662f234f773f8b1742f4d
Gerzon Z <gerzonzcanario@gmail.com>
2022-09-25 12:45:00 -0400

    [CHORE] Setup API (#2)
    * [CHORE] Setup `react-native-navigation` (#1)
    
    * `chore:` setup react-native-navigation
    
    * `chore:` add return type to `PriceDetail` screen
    
    * `chore:` create basic setup for axios client

---
+++ src/api/endpoints.ts
@@ -0,0 +1,48 @@
+import { Alert } from 'react-native';
+import { AxiosResponse } from 'axios';
+
+import fetcher from './fetcher';
+import { IAllEcoIndicators, IEcoIndicator } from '../definitions/rest';
+
+export const getAll: () => Promise<void | IEcoIndicator[]> = () =>
+  fetcher
+    .get('/')
+    .then((response: AxiosResponse<IAllEcoIndicators>) => {
+      const {
+        uf,
+        ivp,
+        dolar,
+        dolar_intercambio,
+        euro,
+        ipc,
+        utm,
+        imanec,
+        tpm,
+        libra_cobre,
+        tasa_desempleo,
+        bitcoin,
+      } = response.data;
+
+      return [
+        { ...uf },
+        { ...ivp },
+        { ...dolar },
+        { ...dolar_intercambio },
+        { ...euro },
+        { ...ipc },
+        { ...utm },
+        { ...imanec },
+        { ...tpm },
+        { ...libra_cobre },
+        { ...tasa_desempleo },
+        { ...bitcoin },
+      ];
+    })
+    .catch(error => Alert.alert('Error', `Ha ocurrido un error: ${error}`));
+
+export const getEconomicIndicator = (indicator: string) => {
+  fetcher
+    .get(`/${indicator}`)
+    .then(response => response.data)
+    .catch(error => Alert.alert('Error', `Ha ocurrido un error: ${error} `));
+};
