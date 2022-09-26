4b9b8e0295b1f1ae645d4ec609be270078d99430
Gerzon Z <gerzonzcanario@gmail.com>
2022-09-25 21:07:47 -0400

    `add:` fetch data for indicator detail

--- src/screens/IndicatorDetail/index.tsx
+++ src/screens/IndicatorDetail/index.tsx
@@ -1,8 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { NavigationFunctionComponent } from 'react-native-navigation';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { getEconomicIndicator } from '../../api/endpoints';
+import { storage } from '../../storage';
+import { IEcoIndicatorDetail } from '../../definitions/rest';
 
-const IndicatorDetail: NavigationFunctionComponent = () => {
+interface Props {
+  code: string;
+}
+
+const IndicatorDetail: NavigationFunctionComponent<Props> = ({ code }) => {
+  const [isDataAvailable, setIsDataAvailable] = useState(false);
+  const { isConnected } = useNetInfo();
+  const [data, setData] = useState<IEcoIndicatorDetail>({});
+
+  const getData = async () => {
+    setIsDataAvailable(true);
+    await getEconomicIndicator(code)
+      .then(response => {
+        storage.set(
+          `ECONOMIC_INDICATOR_${code.toUpperCase()}`,
+          JSON.stringify(response)
+        );
+        setData(response as IEcoIndicatorDetail);
+      })
+      .catch(error => {
+        Alert.alert('Error', `Ha ocurrido un error: ${error}`);
+      });
+  };
+
+  useEffect(() => {
+    if (!isConnected) {
+      const storedData = storage.getString(
+        `ECONOMIC_INDICATOR_${code.toUpperCase()}`
+      );
+      const json = storedData ? JSON.parse(storedData) : null;
+      if (json) {
+        setIsDataAvailable(true);
+        setData(json);
+      }
+    } else {
+      getData();
+    }
+  }, []);
   return (
     <View>
       <Text>IndicatorDetailScreen</Text>
